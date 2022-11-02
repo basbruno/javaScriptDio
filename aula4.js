738e4c144a1aead42a045a543c4acff4970337ca
unknown <brunovoss58@gmail.com>
2022-11-02 11:30:26 -0300

    Adicionado aula04 e 06

---
+++ aula4.js
@@ -0,0 +1,32 @@
+function sayMyName(name){
+
+    console.log('meu nome é : ' + name);
+
+}
+/*sayMyName('Bruno');
+sayMyName('Ester');*/
+
+
+
+function JurosIncrement(valor, percentualJuros){
+
+    const valorDeAcrecimo = (percentualJuros/100) * valor;
+    return valor + valorDeAcrecimo;
+
+}
+/*console.log(JurosIncrement(100,10));
+console.log(JurosIncrement(100,19));
+console.log(JurosIncrement(100,26));
+*/
+
+
+
+
+function main(){
+    console.log('programa principal');
+
+
+
+}
+
+main
---
+++ aula6.js
@@ -0,0 +1,11 @@
+const nome = 'Bruno Araújo da silva'
+
+
+
+for(let i = 0; i < nome.length; i++){
+
+const letra = nome[i];
+console.log(letra);
+
+}
+
